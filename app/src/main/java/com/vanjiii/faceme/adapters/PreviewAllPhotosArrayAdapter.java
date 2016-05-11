0326c57514c052321da7b1933df103eb8c39fa22
vanjiii <ivan.dimitrov90@gmail.com>
2016-05-11 22:51:00 +0300

    Add PreviewAllPhotos fragment and its element

--- app/src/main/java/com/vanjiii/faceme/adapters/PreviewAllPhotosArrayAdapter.java
+++ app/src/main/java/com/vanjiii/faceme/adapters/PreviewAllPhotosArrayAdapter.java
@@ -1,18 +1,44 @@
 package com.vanjiii.faceme.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import com.vanjiii.faceme.R;
 import com.vanjiii.faceme.beans.Person;
 
-import java.util.ArrayList;
+import java.util.List;
 
 /**
+ * Adapter visualizing all persons.
+ *
  * Created by vanjiii on 08.05.16.
  */
 public class PreviewAllPhotosArrayAdapter extends ArrayAdapter<Person> {
 
-    public PreviewAllPhotosArrayAdapter(Context context, ArrayList<Person> persons) {
+    public PreviewAllPhotosArrayAdapter(Context context, List<Person> persons) {
         super(context, -1, persons);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        Person person = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.person_picture_row, parent, false);
+        }
+        // Lookup view for data population
+        TextView name = (TextView) convertView.findViewById(R.id.person_name_row_element_text_view);
+        TextView age = (TextView) convertView.findViewById(R.id.person_age_row_element_text_view);
+        // Populate the data into the template view using the data object
+        name.setText(person.getName());
+        age.setText(person.getAge());
+
+        // Return the completed view to render on screen
+        return convertView;
     }
 }
--- app/src/main/java/com/vanjiii/faceme/fragments/PreviewAllPhotosFragment.java
+++ app/src/main/java/com/vanjiii/faceme/fragments/PreviewAllPhotosFragment.java
@@ -5,24 +5,59 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.vanjiii.faceme.R;
+import com.vanjiii.faceme.adapters.PreviewAllPhotosArrayAdapter;
+import com.vanjiii.faceme.beans.Person;
+
+import java.util.List;
 
 /**
  * Fragment that shows all of the taken photos
- *
+ * <p>
  * Created by vanjiii on 08.05.16.
  */
-public class PreviewAllPhotosFragment extends Fragment{
+public class PreviewAllPhotosFragment extends Fragment {
 
+    private Button sendAllButton;
+    private ListView rowListView;
+
+    View.OnClickListener sendAllPicturesListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getActivity(), "Send All pictures", Toast.LENGTH_LONG).show();
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.preview_all_photos_fragment, parent, false);
-//
-//        initLayoutElements(rootView);
-//        setOnClickListeners();
+
+        initLayoutElements(rootView);
+        setOnClickListeners();
+        populateRow();
 
         return rootView;
+    }
+
+    private void initLayoutElements(View view) {
+        sendAllButton = (Button) view.findViewById(R.id.send_all_pictures_button);
+        rowListView = (ListView) view.findViewById(R.id.picture_row_list_view);
+    }
+
+    private void setOnClickListeners() {
+        sendAllButton.setOnClickListener(sendAllPicturesListener);
+    }
+
+    private void populateRow() {
+        // Construct the data source
+        List<Person> arrayOfUsers = Person.getAllPersons();
+        // Create the adapter to convert the array to views
+        PreviewAllPhotosArrayAdapter adapter = new PreviewAllPhotosArrayAdapter(getContext(), arrayOfUsers);
+        // Attach the adapter to a ListView
+        rowListView.setAdapter(adapter);
     }
 }
